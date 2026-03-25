7bb3aa473a9f025034c160f222f9ca23a02b4258
leonor-code727 <leonordez@yahoo.com>
2026-03-25 15:51:18 -0400

    updating

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 function openMenu() {
   document.body.classList += " menu--open"
+  console.log('menu--open');
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu--open')
-}
+  document.body.classList.remove('menu--open');
+  }
